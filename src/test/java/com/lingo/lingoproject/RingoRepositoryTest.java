15b21ca55b827f12ccfdc29d0ce517c73f957c72
JaeYunChung <jychung2600@gmail.com>
2025-11-23 17:15:14 +0900

    밴 유저 테스트

--- src/test/java/com/lingo/lingoproject/RingoRepositoryTest.java
+++ src/test/java/com/lingo/lingoproject/RingoRepositoryTest.java
@@ -1,11 +1,23 @@
 package com.lingo.lingoproject;
 
 
+import com.lingo.lingoproject.match.MatchService;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("test")
+@ActiveProfiles("dev")
 public class RingoRepositoryTest {
 
+  @Autowired
+  MatchService matchService;
+
+  @Test
+  public void getSuspendedUserTest(){
+    List<Long> banIds = matchService.getBannedRecommendedUserList(5L);
+    System.out.println("-----밴당한 유저 아이디 : " + banIds);
+  }
 }
