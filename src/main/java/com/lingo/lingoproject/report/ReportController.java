4bbb7a2a4c858fa1241ca3c272e25f7eab1e14ee
JaeYunChung <jychung2600@gmail.com>
2025-11-23 17:19:30 +0900

    신고 서비스 보안 문제 해결

--- src/main/java/com/lingo/lingoproject/report/ReportController.java
+++ src/main/java/com/lingo/lingoproject/report/ReportController.java
@@ -1,10 +1,13 @@
 package com.lingo.lingoproject.report;
 
+import com.lingo.lingoproject.domain.User;
 import com.lingo.lingoproject.report.dto.SaveReportRequestDto;
 import com.lingo.lingoproject.utils.ResultMessageResponseDto;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,8 +20,12 @@ public class ReportController {
 
   @PostMapping("/reports")
   public ResponseEntity<ResultMessageResponseDto> report(
-      @Valid @RequestBody SaveReportRequestDto dto
+      @Valid @RequestBody SaveReportRequestDto dto,
+      @AuthenticationPrincipal User user
   ){
+    if (!dto.reportUserId().equals(user.getId())){
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ResultMessageResponseDto("신고할 권한이 없습니다."));
+    }
     reportService.report(dto);
     return ResponseEntity.ok().body(new ResultMessageResponseDto("신고가 성공적으로 접수되었습니다."));
   }
